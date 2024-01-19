e3ecba07a716277eeb19a3ac8527be3d6564f4f3
aliraza11221 <157147994+aliraza11221@users.noreply.github.com>
2024-01-19 22:13:32 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['+923023594323', 'aliraza11221', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
